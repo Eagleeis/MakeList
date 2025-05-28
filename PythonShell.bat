a406ee84a36a235f5cec77f4e732131d5f26bc3e
Jan Blumenthal <eagleeis@gmx.de>
2025-05-29 00:21:38 +0200

    Python path unified.

--- PythonShell.bat
+++ PythonShell.bat
@@ -1,4 +1,4 @@
-set PYTHON_HOME=c:\Tools\Python\3.13
+set PYTHON_HOME=c:\Tools\Python\3.13.1
 doskey py=%PYTHON_HOME%\python.exe $*
 @echo Enter the following to start your script:
 @echo py ^<script^>
